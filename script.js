dfd8a0baa5f8f2564ea8edb5383aa483def50006
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-19 20:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const result = [];
+
+  function backtrack(currentString, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(currentString);
+      return;
+    }
+
+    const letters = digitToLetters[nextDigits[0]];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(currentString + letter, nextDigits.slice(1));
+    }
+  }
+
+  backtrack('', digits);
+  return result.sort();
 }
 
 module.exports = letterCombinations;
